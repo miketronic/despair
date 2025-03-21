431e7e86defce499b11e93eae9718479f46b4bff
mike barnoski <mikebarnoski@gmail.com>
2025-03-21 12:45:59 -0500

    Add files via upload

--- vial/despair_right_vial/keymaps/vial/keymap.c
+++ vial/despair_right_vial/keymaps/vial/keymap.c
@@ -8,12 +8,16 @@ enum layers {
   _ALPHA,
   _NUM,
   _BTB,
-  _FN
+  _MSE,
+  _FN,
+  _SIX,
+  _SVN
 };
 
 // Layer macros
 #define ZFN     LT(_FN, KC_Z)           // Tap for "Z", hold for FN layer
 #define TNUM    LT(_NUM, KC_T)          // Tap for "T", hold for Numbers layer
+#define HMSE    LT(_MSE, KC_H)          // Tap for "H", hold for Mouse layer
 #define BTB     LT(_BTB, KC_B)          // Tap for "B", hold for Trackball layer
 #define SFTSPC  SFT_T(KC_SPC)           // Tap for "Space", hold for Shift
 #define SFTENT  SFT_T(KC_ENT)           // Tap for "Enter", hold for Shift
@@ -37,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ALPHA] = LAYOUT (
    KC_Q,  KC_W,  KC_E,  KC_R,  TNUM,  KC_Y,  KC_U,  KC_I,  KC_O,   KC_P,
-   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,   KC_SCLN,
+   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  HMSE,  KC_J,  KC_K,  KC_L,   KC_SCLN,
    ZFN,   KC_X,  KC_C,  KC_V,  BTB,   KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,
    SFTSPC, SFTSPC,  KC_BTN1,  KC_BTN3,  KC_BTN3, KC_BTN1, SFTENT,  SFTENT
   ),
@@ -56,12 +60,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    SFTSPC, SFTSPC,  KC_BTN1,  KC_BTN3,  KC_BTN3, KC_BTN1, SFTENT,  SFTENT
   ),
 
+  [_MSE] = LAYOUT (
+    XXXX, XXXX,    KC_WH_U,  KC_MS_U,  KC_WH_D,    KC_MINS,KC_UNDS, XXXX, XXXX,   XXXX,
+    XXXX, XXXX,    KC_MS_L,  KC_MS_D,  KC_MS_R,    XXXX,     XXXX, XXXX, XXXX,    XXXX,
+    XXXX, XXXX,    KC_BTN3,  KC_BTN2,  KC_BTN1,    XXXX,     XXXX, XXXX, XXXX,    XXXX,
+    SFTSPC, SFTSPC,  KC_BTN1,  KC_BTN3,  KC_BTN3, KC_BTN1, SFTENT,  SFTENT
+   ),
+
   [_FN] = LAYOUT (
    XXXX, KC_TAB, XXXX,      CLIP,    SS,        KC_F2,    KC_UP,   KC_DEL,  KC_BSPC,   XXXX,
    XXXX, XXXX,   KC_TAB,    XXXX,    XXXX,    KC_LEFT,  KC_DOWN, KC_RGHT, KC_DEL,    XXXX,
    XXXX, XXXX,   XXXX,    DELWORD,   XXXX,    XXXX,     XXXX,   XXXX,      XXXX,      XXXX,
    SFTSPC,  SFTSPC, XXXX,   TSKMGR,  TSKMGR,  XXXX,   SFTENT,  SFTENT
-  )
+  ),
+
+  [_SIX] = LAYOUT (
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    SFTSPC, SFTSPC,  KC_BTN1,  KC_BTN3,  KC_BTN3, KC_BTN1, SFTENT,  SFTENT
+   ),
+
+   [_SVN] = LAYOUT (
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX,  XXXX, XXXX,
+    SFTSPC, SFTSPC,  KC_BTN1,  KC_BTN3,  KC_BTN3, KC_BTN1, SFTENT,  SFTENT
+   )
 
 };
 
